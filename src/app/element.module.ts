1f22383f2c6ee93ad78a51a13f923c1456f2e06e
patrick1729 <prateek.m046@gmail.com>
2022-07-04 21:40:50 +0530

    feat: Add Angular Element configuration and demo folder

---
+++ src/app/element.module.ts
@@ -0,0 +1,34 @@
+import { ApplicationRef, DoBootstrap, Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+import { BrowserModule } from '@angular/platform-browser';
+import { LoginComponent } from './login/login.component';
+
+import { LoginModule } from './login/login.module';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LoginModule
+    ],
+    providers: [],
+})
+export class ElementModule implements DoBootstrap {
+
+    constructor(
+        private injector: Injector
+    ) {}
+
+    ngDoBootstrap(appRef: ApplicationRef) {
+        if (!customElements.get('login-provider')) {
+            // Register only if 'login-provider' entry is not found in the registry
+
+            // Step 3: loginComponent stores the constructor class
+            const loginComponent = createCustomElement(LoginComponent, {
+                injector: this.injector,    // This injector is used to load the component's factory
+            });
+
+            // Step 4: Registering custom tag 'login-provider' with the obtained custom class
+            customElements.define('login-provider', loginComponent);
+        }
+    }
+}
---
+++ src/main.element.ts
@@ -0,0 +1,13 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { ElementModule } from './app/element.module';
+import { environment } from './environments/environment';
+import 'zone.js';
+
+if (environment.production) {
+    enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(ElementModule)
+    .catch(err => console.error(err));
